c9b846de599838f6a4868e2207227e1c46dc6bc2
afdol-LN <afdolleenud63@gmail.com>
2026-07-08 10:35:14 +0700

    [feat] creaet hash rule func

--- libs/hash.js
+++ libs/hash.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const sha256Hash = (data) => {
+    return crypto.createHash('sha256').update(input).digest('hex');
+}
 
 module.exports = {
     async encode(noHashData){
-        const hashedData = await bcrypt.hashSync(noHashData, saltRounds);
-        return hashedData;
+        const result = sha256Hash(noHashData);
+        return result;
     }
-
-    
 }
